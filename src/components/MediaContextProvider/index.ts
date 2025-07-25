765a527924114e63410e72f868362cd4bc016d3e
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2025-07-25 11:53:40 +0300

    mochalov  - Правка структуры;

--- src/components/MediaContextProvider/index.ts
+++ src/components/MediaContextProvider/index.ts
@@ -1,4 +1,4 @@
 export {MediaContext} from "./constants"
 export {useMediaContext} from "./hooks"
-export {MediaContextProvider} from "./MediaContext"
+export {MediaContextProvider} from "./MediaContextProvider"
 export type {IMediaContext, MediaContextProviderProps, TBreakpoints} from "./types"
