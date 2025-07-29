d20f038b80dbecf1d9f8cef947ec5c2d0f461a12
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2025-07-29 10:23:47 +0300

    mochalov  - Фикс подключения шрифтов;

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,49 @@
 import react from "@vitejs/plugin-react";
-import {resolve} from "path";
-import {defineConfig} from "vite";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import svgr from "vite-plugin-svgr";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/components/index.ts"),
-            name: "test-stpr-ui-kit",
-            fileName: (format) => `test-stpr-ui-kit.${format}.js`,
-        },
-        rollupOptions: {
-            external: ["react", "react-dom", "react/jsx-runtime"],
-            output: {
-                globals: {
-                    react: "React",
-                    "react-dom": "ReactDOM",
-                    "react/jsx-runtime": "jsxRuntime"
-                },
-            },
-        },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components/index.ts"),
+      name: "test-stpr-ui-kit",
+      fileName: (format) => `test-stpr-ui-kit.${format}.js`,
     },
-    plugins: [
-        react(),
-        svgr(),
-        dts({
-            insertTypesEntry: true,
-            exclude: ["**/*.stories.ts"],
-            rollupTypes: true,
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+      plugins: [
+        copy({
+          targets: [
+            {
+              src: "src/assets/fonts/*",
+              dest: "dist/assets/fonts",
+            },
+          ],
+          hook: "writeBundle",
+          copyOnce: true,
         }),
-    ],
+      ],
+    },
+  },
+  plugins: [
+    react(),
+    svgr(),
+    dts({
+      insertTypesEntry: true,
+      exclude: ["**/*.stories.ts"],
+      rollupTypes: true,
+    }),
+    copy(),
+  ],
 });
