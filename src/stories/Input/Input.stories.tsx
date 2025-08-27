82497eafc637efd3347c9c78d891437b93522551
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2025-08-27 14:12:12 +0300

    mochalov  - Расширение stories компонента Input для более удобного просмотра состояний компонента;

--- src/stories/Input/Input.stories.tsx
+++ src/stories/Input/Input.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from "@storybook/react";
 import React, { useState } from "react";
 
 import { Input } from "../../../lib/components/Input";
+import { Text } from "../../../lib/components/Text";
+import styles from "./InputStories.module.scss";
 
 const meta: Meta<typeof Input> = {
   component: Input,
@@ -83,13 +85,41 @@ export const Default: Story = {
       }));
     };
     return (
-      <Input
-        {...args}
-        error={!formData.description ? args.error : undefined}
-        name={"description"}
-        value={formData.description}
-        onChange={onChange}
-      />
+      <div className={styles.storiesWrapper}>
+        <Input
+          {...args}
+          error={!formData.description ? args.error : undefined}
+          name={"description"}
+          value={formData.description}
+          onChange={onChange}
+        />
+        <div className={styles.viewInputList}>
+          <div className={styles.viewInputListItem}>
+            <Text type={"description"} classNameRoot={styles.viewInputListItem__description}>
+              Input with Error view
+            </Text>
+            <Input
+              {...args}
+              name={"description"}
+              value={formData.description}
+              error={"Какая-то ошибка"}
+              onChange={(event) => event.stopPropagation()}
+            />
+          </div>
+          <div className={styles.viewInputListItem}>
+            <Text type={"description"} classNameRoot={styles.viewInputListItem__description}>
+              Disabled Input view
+            </Text>
+            <Input
+              {...args}
+              name={"description"}
+              value={formData.description}
+              disabled={true}
+              onChange={(event) => event.stopPropagation()}
+            />
+          </div>
+        </div>
+      </div>
     );
   },
   args: {
@@ -110,15 +140,5 @@ export const WithLabelAndTooltip: Story = {
   args: {
     label: "Описание строения",
     infoTooltipText: "Подсказка для поля 'Описание строения'",
-  },
-};
-export const WithLabelAndTooltipAndError: Story = {
-  name: "Input with label, tooltip and error",
-  render: Default.render,
-  args: {
-    label: "Описание строения",
-    infoTooltipText: "Подсказка для поля 'Описание строения'",
-    required: true,
-    error: "Обязательное поле",
   },
 };
