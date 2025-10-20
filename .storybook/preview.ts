4477e830f3afef4cdbb712203ff1a418756987b9
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2025-10-20 10:22:03 +0300

    develop-mochalov  - Добавление общей документации к сторибуку;

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,17 +1,17 @@
 import type { Preview } from "@storybook/react-vite";
-import { name, version } from "./../package.json";
+import { version } from "./../package.json";
 
 const preview: Preview = {
   parameters: {
     version: {
-      major: `${name}: version ${version.split(".")[0]}`,
+      major: `Version: ${version.split(".")[0]}`,
       minor: version.split(".")[1],
       patch: version.split(".")[2],
-      prefix: `${name}`,
       style: {
         color: "#036bfd",
-        "font-weight": "900",
-        "font-size": "24px",
+        "font-family": "'ALSHauss', Verdana, Arial, Sans-serif;",
+        "font-weight": "400",
+        "font-size": "14px",
       },
     },
     layout: "centered",
