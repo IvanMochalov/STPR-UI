3d90c97cf7c9ddd776c3e3e1ff1c3a824fedebdc
Ivan Mochalov <mochalov.ivan2015@mail.ru>
2025-12-10 13:47:34 +0300

    develop-mochalov  - fix компонента Modal;

--- lib/components/Layer/Layer.tsx
+++ lib/components/Layer/Layer.tsx
@@ -24,26 +24,12 @@ export const Layer: React.FC<LayerProps> = (props) => {
   });
 
   useEffect(() => {
-    // Запоминаем текущую позицию скролла
-    const scrollY = window.scrollY;
     const body = document.body;
 
-    // Блокируем скролл и фиксируем позицию
-    body.style.top = `-${scrollY}px`;
-    body.style.left = "0";
-    body.style.right = "0";
     body.style.overflow = "hidden";
 
     return () => {
-      // Восстанавливаем скролл
-      body.style.position = "";
-      body.style.top = "";
-      body.style.left = "";
-      body.style.right = "";
       body.style.overflow = "";
-
-      // Восстанавливаем позицию скролла
-      window.scrollTo(0, scrollY);
     };
   }, []);
 
